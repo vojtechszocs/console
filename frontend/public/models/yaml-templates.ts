d7be33b920a4a51f998227d0965a5c8a1991a4b0
Vojtech Szocs <vszocs@redhat.com>
2019-01-21 19:08:59 +0100

    Add YAML template for VirtualMachineModel

--- frontend/public/models/yaml-templates.ts
+++ frontend/public/models/yaml-templates.ts
@@ -767,4 +767,43 @@ spec:
     delayAfterAdd: 10s
     delayAfterDelete: 10s
     delayAfterFailure: 10s
+`).setIn([referenceForModel(k8sModels.VirtualMachineModel), 'default'], `
+apiVersion: ${k8sModels.VirtualMachineModel.apiGroup}/${k8sModels.VirtualMachineModel.apiVersion}
+kind: VirtualMachine
+metadata:
+  name: example
+spec:
+  running: false
+  template:
+    metadata:
+      labels:
+        kubevirt.io/size: small
+    spec:
+      domain:
+        devices:
+          disks:
+            - name: registrydisk
+              volumeName: registryvolume
+              disk:
+                bus: virtio
+            - name: cloudinitdisk
+              volumeName: cloudinitvolume
+              disk:
+                bus: virtio
+          interfaces:
+          - name: default
+            bridge: {}
+        resources:
+          requests:
+            memory: 64M
+      networks:
+      - name: default
+        pod: {}
+      volumes:
+        - name: registryvolume
+          registryDisk:
+            image: kubevirt/cirros-registry-disk-demo
+        - name: cloudinitvolume
+          cloudInitNoCloud:
+            userDataBase64: SGkuXG4=
 `);
